37f7ac4a4c45f3d4d14589e70909eb2cbeffd73b
popo <66816259@qq.com>
2015-06-26 14:12:14 +0800

    poposlides

--- js/poposlides.js
+++ js/poposlides.js
@@ -11,7 +11,7 @@
         var settings = $.extend({
             auto: true,             //自动播放
             nav: true,              //切换按钮
-            playspeed: 3500,         //自动播放速度
+            playspeed: 3500,        //自动播放速度
             fadespeed: 500,         //淡入淡出速度
             loop: true,             //循环播放
             pagination: true,       //页码显示
